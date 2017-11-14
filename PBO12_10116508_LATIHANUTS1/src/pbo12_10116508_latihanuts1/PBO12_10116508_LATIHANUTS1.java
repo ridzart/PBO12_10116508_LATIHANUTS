ca1df739c4bbde965f98496c9251cd5b8699a81b
ridzart <32485104+ridzart@users.noreply.github.com>
2017-11-14 22:12:47 +0700

    Add files via upload

---
+++ PBO12_10116508_LATIHANUTS1/src/pbo12_10116508_latihanuts1/PBO12_10116508_LATIHANUTS1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo12_10116508_latihanuts1;
+
+/**
+ *
+ * @author Harrid Alfasya
+ */
+
+class mahasiswa{
+    String nama;
+    String nim;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+    
+}
